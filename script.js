2c9f9da0582e6f30f8d387e068b53c96258d73e5
Prabhjot Singh <singh20prabh@gmail.com>
2023-12-08 13:14:26 +0530

    Css fix

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-const wrapper = document.querySelector(".wrapper");
 const carousel = document.querySelector(".carousel");
 const firstCardWidth = carousel.querySelector(".card").offsetWidth;
 const arrowBtns = document.querySelectorAll(".wrapper i");
-const carouselChildrens = [...carousel.children];
-
-
-let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
-
-carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
-    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
-});
-
 
 
 arrowBtns.forEach(btn => {
@@ -20,16 +10,5 @@ arrowBtns.forEach(btn => {
 });
 
 
-const infiniteScroll = () => {
-    if(carousel.scrollLeft === 0) {
-        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
-    }
-    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
-        carousel.scrollLeft = carousel.offsetWidth;
-    }
-
-}
 
 
-
-carousel.addEventListener("scroll", infiniteScroll);
